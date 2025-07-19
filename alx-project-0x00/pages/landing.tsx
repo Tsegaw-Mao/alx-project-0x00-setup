167c7b74a8c7b71dca14454bd05af7a131961a7b
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-19 09:33:25 +0300

    Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -11,21 +11,21 @@ const Landing: React.FC = () => {
                 {/* Small Buttons */}
                 <Button title="Small Rounded-sm" styles="text-sm rounded-sm" />
                 <Button title="Small Rounded-md" styles="text-sm rounded-md" />
-                <Button title="Small Rounded-full" styles="text-sm rounded-full" />
+                <Button title="Small Rounded-full" styles="text-sm rounded-lg" />
             </div>
 
             <div className="space-x-4">
                 {/* Medium Buttons */}
                 <Button title="Medium Rounded-sm" styles="text-base rounded-sm" />
                 <Button title="Medium Rounded-md" styles="text-base rounded-md" />
-                <Button title="Medium Rounded-full" styles="text-base rounded-full" />
+                <Button title="Medium Rounded-full" styles="text-base rounded-lg" />
             </div>
 
             <div className="space-x-4">
                 {/* Large Buttons */}
                 <Button title="Large Rounded-sm" styles="text-lg rounded-sm" />
                 <Button title="Large Rounded-md" styles="text-lg rounded-md" />
-                <Button title="Large Rounded-full" styles="text-lg rounded-full" />
+                <Button title="Large Rounded-full" styles="text-lg rounded-lg" />
             </div>
         </div>
     )
