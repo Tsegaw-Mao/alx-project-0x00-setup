a37659eb3109f26c0dc8f63d1a8263757148eee3
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-19 09:26:42 +0300

    Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,32 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
     return (
         <div>
-            <h1 className = "text-xl font-extralight"> Landing Page </h1>
+            <h1 className="text-xl font-extralight"> Landing Page </h1>
             <Card />
+
+            <div className="space-x-4">
+                {/* Small Buttons */}
+                <Button title="Small Rounded-sm" styles="text-sm rounded-sm" />
+                <Button title="Small Rounded-md" styles="text-sm rounded-md" />
+                <Button title="Small Rounded-full" styles="text-sm rounded-full" />
+            </div>
+
+            <div className="space-x-4">
+                {/* Medium Buttons */}
+                <Button title="Medium Rounded-sm" styles="text-base rounded-sm" />
+                <Button title="Medium Rounded-md" styles="text-base rounded-md" />
+                <Button title="Medium Rounded-full" styles="text-base rounded-full" />
+            </div>
+
+            <div className="space-x-4">
+                {/* Large Buttons */}
+                <Button title="Large Rounded-sm" styles="text-lg rounded-sm" />
+                <Button title="Large Rounded-md" styles="text-lg rounded-md" />
+                <Button title="Large Rounded-full" styles="text-lg rounded-full" />
+            </div>
         </div>
     )
 }
